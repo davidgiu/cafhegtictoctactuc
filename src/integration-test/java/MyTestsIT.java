1ab07c0c8ddf1be125fa6b9b1a7cf67f1bdc674f
Rafalinho <rafael.moreiradossantos@he-arc.ch>
2025-03-17 11:48:58 +0100

    Add new directories

---
+++ src/integration-test/java/MyTestsIT.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class MyTestsIT {
+    @Test
+    public void testAlwaysPassing() {
+        assertTrue(1 == 1);
+    }
+}
